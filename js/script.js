3e76c4790be8f348fb1ed49e33af3576edbbb277
CarlosBF <cborrellf@outlook.es>
2025-04-03 11:47:13 +0200

    Bonus: List of saved pokemon with deleteAll option

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* VARIABLES */
+
 let offset = 0
 let limit = 10
 const app = document.getElementById('app')
@@ -7,6 +9,7 @@ const prev = document.getElementById('prevBtn')
 const next = document.getElementById('nextBtn')
 const reset = document.getElementById('resetBtn')
 const saved = document.getElementById('saved')
+const delBtn = document.getElementById('delete')
 
 if(localStorage.getItem('savedPokemon') == null){
     localStorage.setItem('savedPokemon', '[]')
@@ -39,6 +42,8 @@ const getPokemon = async (pokemonURL) => {
     }
 }
 
+/* FUNCTIONS */
+
 function getInfo(pokemons) {
     let text = ''
     let promises = []
@@ -55,31 +60,66 @@ function getInfo(pokemons) {
 }
 
 function createDiv(pokemon) {
-    return `
-            <div class="pokemon" onclick="savePokemon('${pokemon.name}')">
-                <img src="${pokemon.sprites.other.home.front_default}">
+    let index = indexOnPokemonList(pokemon)
+    if(index != -1){
+        let pokemon = JSON.parse(localStorage.getItem('savedPokemon'))[index]
+        return `
+            <div class="pokemon rojo" onclick="savePokemon('${pokemon.name}')" id="${pokemon.name}">
+            <svg>
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+            </svg>
+                <img src="${pokemon.image}">
                 <h4 class="name">#${pokemon.id} ${pokemon.name}</h4>
             </div>
             `
+    }else{
+        return `
+            <div class="pokemon" onclick="savePokemon('${pokemon.name}')" id="${pokemon.name}">
+            <svg>
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+            </svg>
+                <img src="${pokemon.sprites.other.home.front_default}">
+                <h4 class="name">#${pokemon.id} ${pokemon.name}</h4>
+            </div>
+        `
+    }
 }
 
 function savePokemon(name) {
+    let elemento = document.getElementById(name)
     let pokemonList = JSON.parse(localStorage.getItem('savedPokemon'))
     getPokemon(`https://pokeapi.co/api/v2/pokemon/${name}/`).then((data) => {
-        let isOnList = false
-        pokemonList.forEach((element) => {
-            if(element.name === data.name){
-                isOnList = true
+        let index = indexOnPokemonList(data)
+        if(index != -1){
+            elemento.classList.remove('rojo')
+            pokemonList.splice(index, 1)
+        }else{
+            elemento.classList.add('rojo')
+            let pokemon = {
+                id:data.id,
+                name:data.name,
+                image:data.sprites.other.home.front_default
             }
-        })
-        if(!isOnList){
-            pokemonList.push(data)
+            pokemonList.push(pokemon)
             pokemonList.sort((a, b) => a.id > b.id)
-            localStorage.setItem('savedPokemon', JSON.stringify(pokemonList))
         }
+        localStorage.setItem('savedPokemon', JSON.stringify(pokemonList))
     })
 }
 
+function indexOnPokemonList(element) {
+    let ans = -1
+    let pokemonList = JSON.parse(localStorage.getItem('savedPokemon'))
+    for (const pokemon of pokemonList) {
+        ans++
+        if(pokemon.id == element.id){
+            return ans
+        }
+    }
+    return -1
+}
+
+/* LISTENERS */
 prev.addEventListener('click', () => {
     if(offset > 0){
         offset -= 10
@@ -96,7 +136,6 @@ next.addEventListener('click', () => {
 })
 
 reset.addEventListener('click', () => {
-    localStorage.setItem('savedPokemon', '[]')
     searchInput.value = ''
     getAll(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
 })
@@ -109,9 +148,8 @@ search.addEventListener('click', () => {
     })
 })
 
-getAll(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
-
 saved.addEventListener('click', () => {
+    delBtn.style.display = ''
     let pokemons = JSON.parse(localStorage.getItem('savedPokemon'))
     let text = ''
     pokemons.forEach((pokemon) => {
@@ -119,3 +157,13 @@ saved.addEventListener('click', () => {
     })
     app.innerHTML = text
 })
+
+delBtn.addEventListener('click', () => {
+    delBtn.style.display = 'none'
+    localStorage.setItem('savedPokemon', '[]')
+    getAll(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+})
+
+/* INITIALIZE */
+
+getAll(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
